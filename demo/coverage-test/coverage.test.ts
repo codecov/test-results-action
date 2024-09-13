a89e7b53a098a4d034b850978ffa4fc3859b08a0
Tom Hu <thomas.hu@sentry.io>
2024-09-13 21:35:23 +0300

    fix: tests

--- demo/coverage-test/coverage.test.ts
+++ demo/coverage-test/coverage.test.ts
@@ -2,10 +2,10 @@ import Coverage from './coverage';
 
 test('test uncovered if', () => {
   const coverageObj = new Coverage();
-  expect(coverageObj.uncovered_if()).toEqual(true);
+  expect(coverageObj.uncovered_if()).toEqual(false);
 });
 
 test('fully covered', () => {
   const coverageObj = new Coverage();
-  expect(coverageObj.fully_covered()).toEqual(false);
+  expect(coverageObj.fully_covered()).toEqual(true);
 });
